54f0e4480089b353a5d1421974127e6d7ba70a21
Zuhayr Smith <suhairsmith17@gmail.com>
2026-06-17 20:14:10 +0200

    updated headshot

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,7 +2,7 @@ import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Code2, Coffee, Rocket, Zap } from "lucide-react";
-import headshot from "../assets/headshot-of-zuhayr-smith.jpg";
+import headshot from "../assets/headshot-of-msmith.jpg";
 
 gsap.registerPlugin(ScrollTrigger);
 
